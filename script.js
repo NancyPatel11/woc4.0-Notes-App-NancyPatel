af99da5a866a2ec1f94ae45ecec826a32b340330
Nancy Patel <202101491@daiict.ac.in>
2022-01-22 19:09:08 +0530

    message

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("plus").addEventListener("click", myfunction);
+function myfunction(){
+let Tasklist = prompt("Please enter Task List:");
+  if(Tasklist!=null){
+    document.getElementById("Tasklist");
+  }
+}
